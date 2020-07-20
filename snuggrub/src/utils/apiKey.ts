8517059e67d9ab679448852068277f069ca73318
Emily <emilyksullivan@outlook.com>
2020-07-20 16:09:10 -0400

    Delete apiKey.ts

--- snuggrub/src/utils/apiKey.ts
+++
@@ -1,7 +0,0 @@
-export const GOOGLE_API_KEY = '<ADD-KEY-HERE>';
-
-export const firebaseConfig = {
-	// FIREBASE API KEY GOES HERE
-};
-
-export const ibmCloudKey = '<IBM-TEXT-TO-SPEECH-CLOUD-KEY>';
